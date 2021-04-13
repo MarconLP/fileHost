c66a033fca55edb8466e8c49846c8458074f8e1c
MarconLP <marcus8910@gmx.de>
2021-04-13 14:39:14 +0200

    make 1 hours file upload limit default and fix joi password validation

--- schemas/joiSchemas.js
+++ schemas/joiSchemas.js
@@ -2,7 +2,7 @@ const Joi = require('joi')
 
 module.exports.uploadSchema = Joi.object({
     passwordCheck: Joi.string().valid('on', 'off'),
-    password: Joi.string(),
+    password: Joi.string().allow(''),
     maxDownloads: [Joi.number().integer().min(1), Joi.string().valid('')],
     storageTime: Joi.number().valid(300, 3600, 86400, 604800, 2628000).required(),
 })
